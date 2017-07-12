76382a28c0fdb37edb6a2d5579b193184923fa50
Graham Ollis <plicease@cpan.org>
2017-07-12 14:49:59 -0400

    Initial commit

---
+++ lib/Alien/Build/Plugin/Probe/GnuWin32.pm
@@ -0,0 +1,9 @@
+package Alien::Build::Plugin::Probe::GnuWin32;
+
+use strict;
+use warnings;
+
+# ABSTRACT: Alien::Build plugin to probe for GnuWin32 packages using the Windows registry
+# VERSION
+
+1;
